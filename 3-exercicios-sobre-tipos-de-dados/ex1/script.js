ba7bf250d6d24172c33a41597076dfc7b416dc87
fernando henrique <fernandomk6@gmail.com>
2022-01-28 18:39:50 -0300

    iniciado seção 3 exercicios sobre tipos de dados, e feito exercicios 1 e 2

---
+++ 3-exercicios-sobre-tipos-de-dados/ex1/script.js
@@ -0,0 +1,4 @@
+console.log("Exercício 1");
+console.log("Olá, meu nome é Fernando Henrique");
+console.log('Olá, meu nome é Fernando Henrique');
+console.log(`Olá, meu nome é Fernando Henrique`);
---
+++ 3-exercicios-sobre-tipos-de-dados/ex2/script.js
@@ -0,0 +1,4 @@
+console.log("Exercício 2");
+console.log(1);
+console.log(1.1);
+console.log(1 + 1);
