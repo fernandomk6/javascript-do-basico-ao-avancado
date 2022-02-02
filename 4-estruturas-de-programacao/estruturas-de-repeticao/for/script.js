a910c5383d6d8ef17d11d5b95ce798feaa10148b
fernando <fernandomk6@gmail.com>
2022-02-02 14:04:53 -0300

    estruturas de repetição while, for

---
+++ 4-estruturas-de-programacao/estruturas-de-repeticao/for/script.js
@@ -0,0 +1,5 @@
+console.log("Estrutura de repetição - for");
+for(let x = 0; x < 10; x = x + 1){
+    console.log(x);
+}
+console.log("loop finalizado");
---
+++ 4-estruturas-de-programacao/estruturas-de-repeticao/while/script.js
@@ -0,0 +1,19 @@
+console.log("Estrutura de repetição - while");
+let x = 10;
+while (x > 0) {
+    console.log(x);
+    x = x - 1;
+}
+let y = 0;
+while(y < 10){
+    console.log(y);
+    y = y + 1;
+}
+
+// do while
+console.log("do while");
+let z = 0;
+do{
+    z = z + 1;
+    console.log(z);
+}while(z < 10);
