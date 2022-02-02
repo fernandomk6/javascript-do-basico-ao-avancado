2bd6a04a0e5aac4a4a76377b7979e9b1e57f63cb
fernando <fernandomk6@gmail.com>
2022-02-02 17:13:33 -0300

    exercicio 1 estruturas de programação

---
+++ 4-estruturas-de-programacao/exercicios/ex1/script.js
@@ -0,0 +1,30 @@
+console.log("Exercício 1");
+/**
+ * string
+ * number
+ * boolean
+ * undefined
+ * null
+ * symbol
+ * bigint
+ * object
+ */
+
+let str = "fernando"; // string
+let numb1 = 1; // number
+let numb2 = 1.1; // number
+let numb3 = -1; // number
+let bool = true; // boolean
+let teste; // undefined
+let N = null; // null, object
+let big = 1n; // bigint
+
+console.log(typeof str);
+console.log(typeof numb1);
+console.log(typeof numb2);
+console.log(typeof numb3);
+console.log(typeof bool);
+console.log(typeof teste);
+console.log(typeof N);
+console.log(typeof big);
+
