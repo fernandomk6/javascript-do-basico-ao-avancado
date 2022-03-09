8d9d0943a976bc52ac8b0343cf4116955e77aaea
fernando henrique <fernandomk6@gmail.com>
2022-03-09 05:39:04 -0300

    configurando projeto e dependencias

--- projetos/jobFinder/app.js
+++ projetos/jobFinder/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const db = require("./db/connection");
 
 app.listen(PORT, function() {
   console.log(`O Express está rodando na porta ${PORT}`);
 });
 
+//db connection
+db
+  .authenticate()
+  .then(() => {
+    console.log("Conectou com sucesso ao banco");
+  })
+  .catch((error) => {
+    console.log("Ocorreu um erro ao conectar: " + error);
+  });
+
+// route
 app.get('/', (req, res) => {
   res.send("Está funcionando");
 });
